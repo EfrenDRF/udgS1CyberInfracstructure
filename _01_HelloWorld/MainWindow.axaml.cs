a4248c1aced4355aa2ec6a116a5f0f62047a433b
Efren Del Real <efren.delrealf@gmail.com>
2021-11-15 11:14:23 -0600

    HelloWorld: message box integraded

--- _01_HelloWorld/MainWindow.axaml.cs
+++ _01_HelloWorld/MainWindow.axaml.cs
@@ -1,17 +1,64 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System.Text.RegularExpressions;
 
 namespace _01_HelloWorld
 {
     public partial class MainWindow : Window
     {
+        private TextBox textPsw1;
+        private TextBox textPsw2;
+        private bool regexMach = false;
+
+        
+
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            textPsw1 = this.FindControl<TextBox>("textPsw");
+            textPsw2 = this.FindControl<TextBox>("textConfPws");
+
+
 #if DEBUG
             this.AttachDevTools();
 #endif
+        }
+        
+        public void button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+
+            if (textPsw1.Text == textPsw2.Text)
+            {
+
+                regexMach = Regex.IsMatch(textPsw1.Text , "[a-z]+");
+
+                if (true == regexMach)
+                {
+                    regexMach = Regex.IsMatch(textPsw1.Text, "[A-Z]+");
+
+                    if (true == regexMach)
+                    {
+                        regexMach = Regex.IsMatch(textPsw1.Text, "[0-9]+");
+                        
+                        regexMach = Regex.IsMatch(textPsw1.Text, "[!-/-:-@-{-~}]+");
+                        
+                        if (true == regexMach)
+                        {
+                            var messageBoxStandardWindow = MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow("Success", "Password has been validated :)");
+                            messageBoxStandardWindow.Show();
+                        }
+                    }
+                }
+ 
+            }
+            
         }
 
         private void InitializeComponent()
