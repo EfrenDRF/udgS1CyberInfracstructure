7dc8ba4948124761960471a0c7b4ce81892e0bc7
Efren Del Real <efren.delrealf@gmail.com>
2021-11-17 22:57:13 -0600

    MySQLConnection: user interface

---
+++ _02_MonoMySQLConnection/MySQLConnecInf/MySQLConnecInf/MainWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Gtk;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void btn_TestConnection_Click(object sender, EventArgs e)
+    {
+
+
+    }
+}
